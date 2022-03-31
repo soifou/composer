ea460db580166295c1fb2009e22dbac92bc91d64
Soifou <fleur.fr@gmail.com>
2022-03-31 15:36:21 +0200

    chore: bump composer releases to latest

--- 8.0/composer.sh
+++ 8.0/composer.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-EXPECTED_SIGNATURE=$(curl https://composer.github.io/installer.sig)
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
 then
     >&2 echo 'ERROR: Invalid installer signature'
     rm composer-setup.php
